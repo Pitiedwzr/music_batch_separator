23ea6e88250c79788e89215327a4dc4a91e4d0c3
Pitiedwzr <2629339380@qq.com>
2024-10-25 12:30:35 +1300

    feat: handle results

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
+import re
 import requests
 import time
 from settings import config
-from file import InputFile
+from file import InputFile, SeparatedFile
+
 
 CREATE_URL = "https://mvsep.com/api/separation/create"
 GET_URL = "https://mvsep.com/api/separation/get"
@@ -28,32 +30,48 @@ def createRequest(audio_file,sep_type,add_opt1=None,add_opt2=None):
     if response_json["success"] == True:
         audio_file.hash = response_json["data"]["hash"]
 
-
     return response, response_json
         
 def checkStatus(audio_file):
-        result_url = GET_URL + "?hash=" + audio_file.hash
-        while audio_file.status != "done":
-            time.sleep(5)
-            response = requests.get(result_url)
-            response_json = response.json()
-            audio_file.status = response_json["status"]
-            print(f"Status: {audio_file.status}")
-            
-        return response, response_json
+    result_url = GET_URL + "?hash=" + audio_file.hash
+    
+    while audio_file.status != "done":
+        time.sleep(5)
+        response = requests.get(result_url)
+        response_json = response.json()
+        audio_file.status = response_json["status"]
+        print(f"Status: {audio_file.status}")    
+        
+    return response, response_json
 
-def downloadFiles(audio_file):
-
-
-audio_file = InputFile("./Fateful Encounter.mp3")
+def downloadFiles(audio_file, response_get_json):
+    '''
+    Download the files from the response
+    '''
+    result_files = SeparatedFile(response_get_json, audio_file)
+        
+    for file_info in result_files.files:
+        response = requests.get(file_info["url"], stream=True)
+        
+        if response.status_code == 200:
+            with open(file_info["path"], 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        file.write(chunk)
+        print(f"File {file_info['name']} downloaded successfully!")
+        
+audio_file = InputFile("./miscs/Fateful Encounter.mp3")
 audio_file.hash = "20241024004516-ef4bdfa430-fateful-encounter.mp3"
 
-try:
+# try:
     #response_post, response_post_json = createRequest(audio_file,40,29)
 
-    response_get, response_get_json = checkStatus(audio_file)
+response_get, response_get_json = checkStatus(audio_file)
+    
+downloadFiles(audio_file, response_get_json)
+
 
     
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+# except Exception as e:
+    # print(f"An error occurred: {str(e)}")
 
--- file.py
+++ file.py
@@ -1,4 +1,6 @@
 import os
+import json
+from settings import config
 
 
 class InputFile:
@@ -12,7 +14,25 @@ class InputFile:
         self.size = os.path.getsize(self.path)
         
 class SeparatedFile:
-    def __init__(self, result):
-        self.size = 
-        self.url = ""
-        self.name = ""
+    def __init__(self, datas, InputFile):
+        if config.common.output_format == 0:
+            format = ".mp3"
+        elif config.common.output_format == 1:
+            format = ".wav"
+        elif config.common.output_format == 2:
+            format = ".flac"
+        elif config.common.output_format == 3:
+            format = ".m4a"
+        
+
+        # Handle both cases: when datas is a list or when it's a dict with "files" key
+        files_list = datas if isinstance(datas, list) else datas.get("files", [])
+        
+        self.files = []  # Store all files info
+        for file_data in files_list:
+            file_info = {
+                "name": InputFile.name + "_" + file_data["type"] + format,
+                "path": "./output/" + InputFile.name + "_" + file_data["type"] + format,
+                "url": file_data["url"]
+            }
+            self.files.append(file_info)
