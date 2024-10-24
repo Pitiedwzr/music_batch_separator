5b935cb22b0a58e6c679ac5ff13b81104ad66d98
Pitiedwzr <2629339380@qq.com>
2024-10-24 14:07:06 +1300

    ui: connect to the function

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
 from PySide6.QtCore import QTranslator, QLocale, QCoreApplication
 from settings import config
+import api
 
 
 class MainWindow(QMainWindow):
+    """Inital the UI and functions"""
     def __init__(self):
         """Inital the UI and functions"""
         super().__init__()
@@ -16,16 +18,22 @@ class MainWindow(QMainWindow):
         self.initSlot()
 
     def initSlot(self):
+        """Initial all slots"""
         self.ui.actionAbout_Qt.triggered.connect(self.showAboutQt)
         self.ui.actionPreferences.triggered.connect(self.jumpToSettings)
+        self.ui.pushButtonProcess.clicked.connect(self.sendRequest)
         
     def showAboutQt(self):
         QApplication.aboutQt()
         
     def jumpToSettings(self):
         settings_dialog.show()
+     
+    def sendRequest(self):
+        api.createRequest()
 
 class SettingsDialog(QDialog):
+    """Inital the UI and functions"""
     def __init__(self):
         """Inital the UI and functions"""
         super().__init__()
@@ -34,14 +42,15 @@ class SettingsDialog(QDialog):
         self.initSlot()
         
     def initSlot(self):
+        """Initial all slots"""
         self.ui.pushButtonSave.clicked.connect(self.saveSettings)
         self.ui.pushButtonCancel.clicked.connect(self.exitSettings)
         self.ui.comboBoxOutputFormat.setCurrentIndex(config.common.output_format)
-        self.ui.checkBoxIsDemo.setChecked(config.common.is_demo)
+        self.ui.checkBoxIsDemo.setChecked(bool(config.common.is_demo))
 
     def saveSettings(self):
         config.common.output_format = self.ui.comboBoxOutputFormat.currentIndex()
-        config.common.is_demo = self.ui.checkBoxIsDemo.isChecked()
+        config.common.is_demo = int(self.ui.checkBoxIsDemo.isChecked())
         config.save()
         
     def exitSettings(self):
